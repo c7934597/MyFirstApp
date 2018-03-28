ddd18890bb9b3b5414ba80ed59974e63dde4a236
林韋銘 <c7934597@gmail.com>
2018-03-28 21:07:35 +0800

    除了單元測試外，還增加了裝置測試程式碼

--- app/src/androidTest/java/com/myandroid/myfirstapp/MainActivityTest.java
+++ app/src/androidTest/java/com/myandroid/myfirstapp/MainActivityTest.java
@@ -1,12 +1,78 @@
 package com.myandroid.myfirstapp;
 
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    //用來儲存Main
+    //用來儲存MainActivity 物件和它的介面元件
+    private MainActivity mMainActivity;
+    private EditText mEdtSex, mEdtAge;
+    private Button mBtnDoSug;
+    private TextView mTxtResult;
 
     public MainActivityTest() {
         super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+
+        //完成初始設定。
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+
+        //利用 getActivity()方法取得 MainActivity物件
+        //再取得MainActivity物件的介面元件
+        mMainActivity = (MainActivity) getActivity();
+        mEdtSex = (EditText) mMainActivity.findViewById(R.id.edtSex);
+        mEdtAge = (EditText) mMainActivity.findViewById(R.id.edtAge);
+        mBtnDoSug = (Button) mMainActivity.findViewById(R.id.btnOK);
+        mTxtResult = (TextView) mMainActivity.findViewById(R.id.txtR);
+    }
+
+    @Test
+    public void maleAge25(){
+        //測試男生，年齡25的情況
+        //對於介面元件的控制必須在UI thread (或稱為 main thread) 中執行
+        mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mEdtSex.setText("男");
+                mEdtAge.setText("25");
+                mBtnDoSug.performClick();
+            }
+        });
+
+        //由於模擬器執行比較慢，等候一點時間讓他執行完畢
+        try {
+            Thread.sleep(2000); //等2秒鐘
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //檢查是否顯示正確的字串
+        String asd = mMainActivity.getString(R.string.sug_not_hurry);
+        String asdd =mTxtResult.getText().toString();
+
+        assertEquals("建議:"+mMainActivity.getString(R.string.sug_not_hurry),mTxtResult.getText().toString());
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        super.tearDown();
+
+        //加入測試完畢後執行清除的程式碼。
     }
 }
